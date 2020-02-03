8db5c2a06cdd26c8bcf772c0ae4995bbf3db5084
alphamikle <alphamikle@gmail.com>
2020-02-03 22:27:15 +0300

    email sender "from" getting from env

--- src/email/content/mock-content.ts
+++ src/email/content/mock-content.ts
@@ -1,5 +1,7 @@
 import { EmailContentEntity } from '../entities/email-content.entity';
 
+const { MAIL_USERNAME } = process.env;
+
 // eslint-disable-next-line no-template-curly-in-string
 export const PASSWORD_VARIABLE = '${password}';
 
@@ -8,7 +10,7 @@ export const RESTORE_ENTITY_CODE = 'restorePassword';
 const RESTORE_ENTITY = EmailContentEntity.createFrom(RESTORE_ENTITY_CODE,
   'Восстановление пароля',
   `Ваш новый пароль: ${ PASSWORD_VARIABLE }`,
-  'info@quich.ru',
+  MAIL_USERNAME,
 );
 
 export const REQUIRED_ENTITIES = [ RESTORE_ENTITY ];
